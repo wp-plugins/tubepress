10c67f3ba84d2db95ff0e83ced3a0e4886fb5d8c
k2eric <k2eric@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2006-10-03 15:18:44 +0000

    Cleaned up formatting. Added some comments.
    git-svn-id: https://plugins.svn.wordpress.org/tubepress/trunk@6576 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- tubepress.php
+++ tubepress.php
@@ -28,16 +28,22 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */ 
 
+
+/*
+ * Main filter hook. Looks for [tubepress] keyword
+ * and replaces it with YouTube gallery if it's found
+*/
 function tubepress_showgallery ($content = '') {
+
+	/* Bail out fast if not found */
 	$keyword = "[tubepress]";
 	if (!strpos($content,$keyword)) return $content;
 
-	$devID = 		get_option('devID');
-	$username = 		get_option('username');
-
-	$youtube_xml = get_youtube_xml($devID, $username); 
+	/* Grab the XML from YouTube's API */
+	$youtube_xml = get_youtube_xml(get_option('devID'), get_option('username')); 
+	
+	/* Loop through each video and generate the HTML for each */
 	$videoCount = 0;
-
 	$newcontent = printHTML_videoheader();
 	foreach ($youtube_xml as $k => $v) {
 		if (is_array($v)) {
@@ -52,64 +58,66 @@ function tubepress_showgallery ($content = '') {
 		$newcontent .= "ERROR: Could not retrieve favorites from YouTube.";
 	}
 	$newcontent .= printHTML_videofooter();
+
+	/* We're done, so let's insert the gallery where the keyword is */
 	return str_replace($keyword, $newcontent, $content);
 }
 
 function printHTML_videoheader() {
-return <<<EOT
-	<div class="tubepress_container">
+	return <<<EOT
+		<div class="tubepress_container">
 EOT;
 }
 
 function printHTML_videofooter() {
-return <<<EOT
-	</div>
-	</div>
+	return <<<EOT
+			</div>
+		</div>
 EOT;
 }
 
 function printHTML_bigvid($vid) {
-	$id = $vid['id'];
-	$title = $vid['title'];
-	$length = humanTime($vid['length_seconds']);
-	$height = get_option('mainVidHeight');
-	$width = get_option('mainVidWidth');
-return <<<EOT
-	<div id="tubepress_the_video" class="tubepress_video_full">
-		<div class="tubepress_meta_large">
-			Latest post: $title ($length)
-		</div>
-		<object width="$width" height="$height">
-			<param name="movie" value="http://www.youtube.com/v/$id" />
-			<embed src="http://www.youtube.com/v/$id" type="application/x-shockwave-flash" width="$width" height="$height" />
-		</object>
-	</div> <!-- tubepress_video_full -->
-	<div class="tubepress_video_thumbs">
+	$id = 		$vid['id'];
+	$title = 	$vid['title'];
+	$length = 	humanTime($vid['length_seconds']);
+	$height = 	get_option('mainVidHeight');
+	$width = 	get_option('mainVidWidth');
+
+	return <<<EOT
+		<div id="tubepress_the_video" class="tubepress_video_full">
+			<div class="tubepress_meta_large">
+				Latest post: $title ($length)
+			</div>
+			<object width="$width" height="$height">
+				<param name="movie" value="http://www.youtube.com/v/$id" />
+				<embed src="http://www.youtube.com/v/$id" type="application/x-shockwave-flash" width="$width" height="$height" />
+			</object>
+		</div> <!-- tubepress_video_full -->
+		<div class="tubepress_video_thumbs">
 EOT;
 }
 
 function humanTime($length_seconds) {
 	/* convert the time to human-friendly */
-        $seconds = $length_seconds;
-        $length = intval($seconds/60);
-        $length .= ":" . $seconds%60;
+        $seconds = 	$length_seconds;
+        $length = 	intval($seconds/60);
+        $length .= 	":" . $seconds%60;
 	return $length;
 }
 
 function printHTML_smallvid($vid) {
-	/* convert the time to human-friendly */
-	$length = humanTime($vid['length_seconds']);
 
+	$length = 		humanTime($vid['length_seconds']);
 	$title = 		$vid['title'];
 	$thumbnail_url = 	$vid['thumbnail_url'];
 	$view_count = 		number_format($vid['view_count']);
 	$id = 			$vid['id'];
 
-	$thumbHeight = get_option('thumbHeight');
-	$thumbWidth = get_option('thumbWidth');
-	$height = get_option('mainVidHeight');
-	$width = get_option('mainVidWidth');
-	$caption = $title . "(" . $length . ")";
+	$thumbHeight = 	get_option('thumbHeight');
+	$thumbWidth = 	get_option('thumbWidth');
+	$height = 	get_option('mainVidHeight');
+	$width = 	get_option('mainVidWidth');
+	$caption = 	$title . "(" . $length . ")";
 
 return <<<EOT
 	<div class="tubepress_video_thumb">
@@ -128,6 +136,10 @@ return <<<EOT
 EOT;
 }
 
+/*
+ * Connects to YouTube and grabs gallery info over
+ * REST API
+*/
 function get_youtube_xml($devID, $username) {
 	$request = "http://www.youtube.com/api2_rest?method=youtube.users.list_favorite_videos&dev_id=" . $devID . "&user=" . $username;
 	$master_node = "video_list";
@@ -139,13 +151,12 @@ function get_youtube_xml($devID, $username) {
 	return (array)$xml->$master_node;
 }
 
-
 function insert_tubepress_js() {
-	echo "\t<script type=\"text/javascript\" src=\"wp-content/plugins/tubepress/tubepress.js\"></script>\n";
+	echo '\t<script type="text/javascript" src="wp-content/plugins/tubepress/tubepress.js"></script>\n';
 }
 
 function insert_tubepress_css() {
-	echo "\t<link rel=\"stylesheet\" href=\"wp-content/plugins/tubepress/tubepress.css\" type=\"text/css\"></link>";
+	echo '\t<link rel="stylesheet" href="wp-content/plugins/tubepress/tubepress.css" type="text/css"></link>';
 }
 
 function tubepress_add_options_page() {
@@ -156,8 +167,6 @@ function tubepress_add_options_page() {
 
 function tubepress_options_subpanel() {
 
-	$errors = 0;
-	$errorText = "";
 	if (isset($_POST['tubepress_save'])) {
 		if (isset($_POST['devID'])) 		update_option('devID', 		$_POST['devID']);
 		if (isset($_POST['username'])) 		update_option('username', 	$_POST['username']);
@@ -249,15 +258,20 @@ print <<<EOT
 EOT;
 
 }
-add_action('admin_menu', 'tubepress_add_options_page');
-add_option("devID", "qh7CQ9xJIIc", 'YouTube developer ID. Available from <a href="http://www.youtube.com/my_profile_dev">http://www.youtube.com/my_profile_dev</a>');
-add_option("username", "3hough", "YouTube username.");
-add_option("mainVidWidth", "425", "Width (px) of main video");
-add_option("mainVidHeight", "350", "Height (px) of main video");
-add_option("thumbWidth", "130", "Width (px) of video thumbnails");
-add_option("thumbHeight", "97", "Height (px) of video thumbnails");
 
-add_action('wp_head', 'insert_tubepress_css');
-add_action('wp_head', 'insert_tubepress_js');
+/* ACTIONS */
+add_action('admin_menu', 	'tubepress_add_options_page');
+add_action('wp_head', 		'insert_tubepress_css');
+add_action('wp_head', 		'insert_tubepress_js');
+
+/* OPTIONS */
+add_option("devID", 		"qh7CQ9xJIIc", 	'YouTube developer ID. Available from <a href="http://www.youtube.com/my_profile_dev">http://www.youtube.com/my_profile_dev</a>');
+add_option("username", 		"3hough", 	"YouTube username.");
+add_option("mainVidWidth", 	"425", 		"Max width (px) of main video");
+add_option("mainVidHeight", 	"350", 		"Max height (px) of main video");
+add_option("thumbWidth", 	"130", 		"Max width (px) of video thumbnails");
+add_option("thumbHeight", 	"97", 		"Max height (px) of video thumbnails");
+
+/* FILTERS */
 add_filter('the_content', 'tubepress_showgallery');
 
