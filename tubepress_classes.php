de567b7aa5c9c0fcd022c766a10100eb8e6348ec
k2eric <k2eric@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2006-11-18 00:36:11 +0000

    Adding version 0.8
    git-svn-id: https://plugins.svn.wordpress.org/tubepress/tags/tubepress_0.8@6908 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tubepress_classes.php
@@ -0,0 +1,83 @@
+<?php
+
+/*
+ * Serves as a constant object to hold CSS info
+*/
+class tubepressCSS {
+	var $mainVid_id, $mainVid_class, $meta_class,
+		$thumb_container_class, $runtime_class, $title_class, $meta_group;
+
+	function tubepressCSS() {
+		$this->container = 		"tubepress_container";
+		$this->mainVid_id = 		"tubepress_mainvideo";
+		$this->mainVid_class = 		"tubepress_mainvideo";
+		$this->meta_class =		"tubepress_meta";
+		$this->thumb_container_class =	"tubepress_video_thumbs";
+		$this->thumb_class =		"tubepress_thumb";
+		$this->thumbImg_class =		"tubepress_video_thumb_img";
+		$this->runtime_class = 		"tubepress_runtime";
+		$this->title_class = 		"tubepress_title";
+		$this->success_class = 		"updated fade";
+		$this->meta_group = 		"tubepress_meta_group";
+	}
+}
+
+class tubepressTag {  
+
+	var $tagString, $customOptions;
+
+	function tubepressTag($tagString, $optionsArray) {  
+		$this->tagString = $tagString;  
+		$this->customOptions = $optionsArray;
+	}  
+
+	function get_option($option = '') {
+		if(!empty($this->customOptions) && isset($this->customOptions[$option]))
+			return $this->customOptions[$option];  
+
+
+		$allOptions = array(get_option(TP_OPTS_META),get_option(TP_OPTS_ACCT),
+				get_option(TP_OPTS_SEARCH), get_option(TP_OPTS_DISP),
+				get_option(TP_OPTS_ADV), get_option(TP_OPTS_SRCHV)); 
+
+		foreach ($allOptions as $optionArray) {
+			foreach ($optionArray as $optionItem) {
+				if ($optionItem->name == $option) return $optionItem->value;
+			}
+		}
+	}  
+} 
+
+class tubepressVideo {
+	var $metaValues;
+		
+	function tubepressVideo($videoXML) {
+		$this->metaValues = array(
+			TP_VID_AUTHOR =>		$videoXML->author->CDATA(),
+		 	TP_VID_ID =>			$videoXML->id->CDATA(),
+		 	TP_VID_TITLE =>			htmlentities($videoXML->title->CDATA(), ENT_QUOTES),
+			TP_VID_LENGTH =>		humanTime($videoXML->length_seconds->CDATA()),
+ 			TP_VID_RATING_AVG =>	$videoXML->rating_avg->CDATA(),
+	 		TP_VID_RATING_CNT =>	number_format($videoXML->rating_count->CDATA()),
+	 		TP_VID_DESC =>			$videoXML->description->CDATA(),
+		 	TP_VID_VIEW =>			number_format($videoXML->view_count->CDATA()),
+	 		TP_VID_UPLOAD_TIME =>	date("M j, Y", $videoXML->upload_time->CDATA()),
+			TP_VID_COMMENT_CNT =>	number_format($videoXML->comment_count->CDATA()),
+			TP_VID_TAGS =>			$videoXML->tags->CDATA(),
+		 	TP_VID_URL =>			$videoXML->url->CDATA(),
+		 	TP_VID_THUMBURL =>		$videoXML->thumbnail_url->CDATA()
+		);
+	}
+}
+
+class tubepressOption {
+	var $name, $title, $description, $value;
+	
+	function tubePressOption($theName, $theTitle, $theDesc, $theValue) {
+		$this->name = $theName;
+		$this->description = $theDesc;
+		$this->value = $theValue;
+		$this->title = $theTitle;
+	}
+}
+?>
---
+++ tubepress_strings.php
@@ -0,0 +1,117 @@
+<?php
+/*
+THANKS:
+This plugin was based on the "mytube" plugin by Vaam Yob (http://rane.hasitsown.com/blog/plink/technical/27/wordpress-youtube-video-gallery-plugin/) and
+some code samples from WaxJelly (http://www.waxjelly.com/2006/08/29/a-more-complex-php-script-using-the-youtube-api-with-video-details-part-2/). Thanks!
+
+Copyright (C) 2007 Eric D. Hough (k2eric@gmail.com)
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/ 
+
+/* Messages */
+define("TP_MSG_OPTPANELTITLE",	"TubePress Options");
+define("TP_MSG_OPTPANELMENU",	"TubePress");
+define("TP_MSG_OPTSUCCESS",	"Options updated");
+define("TP_MSG_TIMEOUT",	"Timed out while contacting YouTube. Please try again later.");
+define("TP_MSG_YTERR",		"Could not retrieve any videos from YouTube. Please check your configuration.");
+define("TP_MSG_WHICHVIDS",	"Which videos?");
+define("TP_MSG_VIDDISP",	"Video display");
+define("TP_MSG_META",		"Video meta display");
+define("TP_MSG_ADV",		"Advanced");
+define("TP_MSG_ACCT",		"Your YouTube Account");
+define("TP_MSG_SAVE",		"Save");
+define("TP_MSG_XMLERR",		"No videos from YouTube!");
+define("TP_MSG_MAINVID_HEADER",	"Latest post:");
+define("TP_MSG_SRCH_FAV_TITLE",	"your favorites");
+define("TP_MSG_SRCH_TAG_TITLE",	"this tag");
+define("TP_MSG_SRCH_USER_TITLE",	"this user");
+define("TP_MSG_SRCH_YV_TITLE",	"your videos");
+define("TP_MSG_DEVID_TITLE",		"YouTube developer ID");
+define("TP_MSG_DEVID_DESC",		"Available from");
+define("TP_MSG_KEYWORD_TITLE",		"Display keyword");
+define("TP_MSG_KEYWORD_DESC",		"The trigger you insert (in plaintext, between square brackets) into your posts to display your YouTube gallery.");
+define("TP_MSG_THUMBHEIGHT_TITLE",	"Height (px) of thumbs");
+define("TP_MSG_THUMBHEIGHT_DESC",  	"Default is 90");
+define("TP_MSG_THUMBWIDTH_TITLE",	"Width (px) of thumbs");
+define("TP_MSG_THUMBWIDTH_DESC",	"Default is 120");
+define("TP_MSG_USERNAME_TITLE",		"YouTube username");
+define("TP_MSG_USERNAME_DESC",  	"");
+define("TP_MSG_VIDHEIGHT_TITLE",	"Max height (px) of main video");
+define("TP_MSG_VIDHEIGHT_DESC", 	"Default is 350");
+define("TP_MSG_VIDWIDTH_TITLE",		"Max width (px) of main video");
+define("TP_MSG_VIDWIDTH_DESC",  	"Default is 425");
+define("TP_MSG_TIMEOUT_TITLE",		"How long to wait (in seconds) for YouTube to respond");
+define("TP_MSG_TIMEOUT_DESC",		"Default is 6 seconds");
+define("TP_MSG_VIDAUTHOR",	"Author");
+define("TP_MSG_VIDID",		"Video ID");
+define("TP_MSG_VIDTITLE",	"Title");
+define("TP_MSG_VIDLEN",		"Length");
+define("TP_MSG_VIDRATINGS",	"Ratings");
+define("TP_MSG_VIDRATING",	"Rating");
+define("TP_MSG_VIDDESC",	"Description");
+define("TP_MSG_VIDVIEWS",	"Views");
+define("TP_MSG_VIDUPLOAD",	"Uploaded");
+define("TP_MSG_VIDCOMMENTS",	"Comments");
+define("TP_MSG_VIDTAGS",	"Tags");
+define("TP_MSG_VIDURL",		"YouTube URL");
+define("TP_MSG_VIDTHUMBURL",	"Thumbnail URL");
+
+/* General */
+define("TP_MASTERNODE", 	"video_list");
+define("TP_YOUTUBEDEVLINK", 	"http://www.youtube.com/my_profile_dev"	);
+define("TP_YOUTUBE_RESTURL",	"http://www.youtube.com/api2_rest?");
+
+/* Searching */
+define("TP_SRCH_FAV",		"favorites");
+define("TP_SRCH_TAG",		"tag");
+define("TP_SRCH_USER",		"user");
+define("TP_SRCH_YV",		"yourvideos");
+define("TP_SRCH_TAGVAL",	"tagValue");
+define("TP_SRCH_USERVAL",	"userValue");
+
+/* OPTIONS */
+define("TP_OPT_DEVID",			"devID");
+define("TP_OPT_KEYWORD", 		"tubepress");
+define("TP_OPT_SEARCHBY", 		"mode");
+define("TP_OPT_THUMBHEIGHT",	"thumbHeight");
+define("TP_OPT_THUMBWIDTH",		"thumbWidth");
+define("TP_OPT_USERNAME",		"username");
+define("TP_OPT_VIDHEIGHT",		"mainVidHeight");
+define("TP_OPT_VIDWIDTH",		"mainVidWidth");
+define("TP_OPT_TIMEOUT",		"timeout");
+define("TP_OPTS_META",			"tubepress_metaOptions");
+define("TP_OPTS_ACCT",			"tubepress_accountInfo");
+define("TP_OPTS_SEARCH",		"tubepress_videoSearchOptions");
+define("TP_OPTS_DISP",			"tubepress_videoDisplayOptions");
+define("TP_OPTS_ADV", 			"tubepress_advancedOptions");
+define("TP_OPTS_SRCHV",			"tubepress_searchVariables");
+
+/* VIDEO META INFO */
+define("TP_VID_AUTHOR",			"author");
+define("TP_VID_ID",				"id");
+define("TP_VID_TITLE",			"title");
+define("TP_VID_LENGTH",			"length");
+define("TP_VID_RATING_CNT",		"ratings");
+define("TP_VID_RATING_AVG",		"rating");
+define("TP_VID_DESC",			"description");
+define("TP_VID_VIEW",			"views");
+define("TP_VID_UPLOAD_TIME",	"uploaded");
+define("TP_VID_COMMENT_CNT",	"comments");
+define("TP_VID_TAGS",			"tags");
+define("TP_VID_URL",			"url");
+define("TP_VID_THUMBURL",		"thumburl");
+
+?>
