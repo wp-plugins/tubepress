31e21a072e3bb294ab306e3c28d7b01bd060195f
k2eric <k2eric@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2007-01-16 01:50:54 +0000

    Minor bug fixes
    git-svn-id: https://plugins.svn.wordpress.org/tubepress/trunk@7359 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- tubepress.js
+++ tubepress.js
@@ -1,7 +1,7 @@
 function playVideo(id, height, width, title, time, location, url) {
 	switch (location) {
 		case "popup":
-			var newurl = url + "/wp-content/plugins/tubepress/tubepress_popup.php?name=" + title + "&id=" + id + "&w=" + width + "&h=" + height;
+			var newurl = url + "/wp-content/plugins/tubepress/tubepress_popup.php?name=" + escape(title) + "&id=" + id + "&w=" + width + "&h=" + height;
 			window.open(newurl, "newwin", "width=" + width + ",height=" + height + ",toolbar=false,locationbar=false,directories=false,status=false,menubar=false,scrollbars=false,resizable=true,copyhistory=false");
 			break;
 		case "youtube":
