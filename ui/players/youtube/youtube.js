86e169bbb18b56dd0d019006178d7a4b71b084a4
k2eric <k2eric@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-12-09 04:14:37 +0000

    Incremental commit for version 1.8.7
    git-svn-id: https://plugins.svn.wordpress.org/tubepress/trunk@181149 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- ui/players/youtube/youtube.js
+++
@@ -1,7 +0,0 @@
-function tubepress_youtube_player(galleryId, videoId) {
-   window.location = "http://www.youtube.com/watch?v=" + videoId;
-}
-
-function tubepress_youtube_player_init(baseUrl) { 
-    //nada
-}
