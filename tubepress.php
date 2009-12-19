69b89e2cf5f9de818ab8b7c5135fde07b48f7911
k2eric <k2eric@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-12-19 22:50:39 +0000

    Version 1.8.8
    git-svn-id: https://plugins.svn.wordpress.org/tubepress/trunk@185064 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tubepress.php
@@ -0,0 +1,32 @@
+<?php
+/**
+Plugin Name: TubePress
+Plugin URI: http://tubepress.org
+Description: Displays gorgeous YouTube galleries in your posts, pages, and/or sidebar. Upgrade to <a href="http://tubepress.org/features/">TubePress Pro</a> for more features!
+Author: Eric D. Hough
+Version: 1.8.8
+Author URI: http://ehough.com
+
+Copyright 2006 - 2010 Eric D. Hough (http://ehough.com)
+
+This file is part of TubePress (http://tubepress.org)
+
+TubePress is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+TubePress is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with TubePress.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+include "env/WordPress/hooks/main.php";
+include "env/WordPress/hooks/options_page.php";
+include "env/WordPress/hooks/widget.php";	
+
+?>
