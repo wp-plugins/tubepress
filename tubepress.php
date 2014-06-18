b10c22aee23be444cd71b04a20e28bffd8e8e9d0
k2eric <k2eric@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-06-18 21:35:31 +0000

    Incremental commit for 3.1.6
    git-svn-id: https://plugins.svn.wordpress.org/tubepress/trunk@934726 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tubepress.php
@@ -0,0 +1,19 @@
+<?php
+/**
+Plugin Name: TubePress
+Plugin URI: http://tubepress.com
+Description: Displays gorgeous YouTube and Vimeo galleries in your posts, pages, and/or sidebar. Upgrade to <a href="http://tubepress.com/pro/">TubePress Pro</a> for more features!
+Author: TubePress LLC
+Version: 3.1.6
+Author URI: http://tubepress.com
+
+Copyright 2006 - 2014 TubePress LLC (http://tubepress.com)
+
+This file is part of TubePress (http://tubepress.com)
+
+This Source Code Form is subject to the terms of the Mozilla Public
+License, v. 2.0. If a copy of the MPL was not distributed with this
+file, You can obtain one at http://mozilla.org/MPL/2.0/.
+*/
+
+include 'src/main/php/scripts/boot.php';
---
+++ vendor/ehough/stash/src/main/php/ehough/stash/exception/Exception.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * This file is part of the Stash package.
+ *
+ * (c) Robert Hafner <tedivm@tedivm.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * Interface for the Stash exceptions.
+ *
+ * @package Stash
+ * @author  Robert Hafner <tedivm@tedivm.com>
+ */
+interface ehough_stash_exception_Exception
+{
+}
